af93b51a4552fe59eaf00996b2861f8301eccf0b
Adson <adsonfr.vieira@hotmail.com>
2026-03-14 17:55:06 -0300

    add : Projeto Inicial  toDo List

---
+++ src/main/java/com/todolist/Todo/Llist/TodoLlistApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.Todo.Llist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoLlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoLlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/Todo/Llist/TodoLlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.Todo.Llist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoLlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
